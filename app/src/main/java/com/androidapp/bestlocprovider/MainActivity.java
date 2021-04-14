42fb39a044c0b47185c503e2b5e92ee8f2db6131
Y-B Park <201921055@sangmyung.kr>
2021-04-14 11:34:34 +0900

    complete onResume

--- app/src/main/java/com/androidapp/bestlocprovider/MainActivity.java
+++ app/src/main/java/com/androidapp/bestlocprovider/MainActivity.java
@@ -1,15 +1,22 @@
 package com.androidapp.bestlocprovider;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     LocationManager locManager;
-    TextView txtInfo;
+    LocationListener locListener;
+    TextView myLocField;
     Criteria criteria;
     String bestProvName;
 
@@ -18,13 +25,39 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        txtInfo = findViewById(R.id.txtInfo);
+        myLocField = findViewById(R.id.myLocationField);
         locManager = (LocationManager)getSystemService(LOCATION_SERVICE);
 
         criteria = new Criteria();
         criteria.setAccuracy(Criteria.ACCURACY_COARSE);
 
-        bestProvName = locManager.getBestProvider(criteria, enabledOnly: true);
-        txtInfo.setText("Best Loc. provider: " + bestProvName);
+        bestProvName = locManager.getBestProvider(criteria, true);
+    //    myLocField.setText("Best Loc. provider: " + bestProvName);
+
+        locListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(@NonNull Location location) {
+                myLocField.setText(
+                        "Latitude: " + location.getLatitude()
+                        + "\nLongtitude: " + location.getLongitude()
+                        + "\nAltitude: " + location.getAltitude());
+            }
+        };
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                !=PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED);
+        locManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locListener);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        locManager.removeUpdates(locListener);
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
     }
 }
